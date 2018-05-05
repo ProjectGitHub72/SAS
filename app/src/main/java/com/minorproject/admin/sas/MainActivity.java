6b4d8138083250bc456efd5a2536fb7a632e16a3
Ris3r <ProjectGitHub72@gmail.com>
2018-05-05 20:07:34 +0545

    Initial Authentication Added.

--- app/src/main/java/com/minorproject/admin/sas/MainActivity.java
+++ app/src/main/java/com/minorproject/admin/sas/MainActivity.java
@@ -1,9 +1,11 @@
 package com.minorproject.admin.sas;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,26 +14,139 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+
+    private static final int RC_SIGN_IN = 1;
+
+   //     TextView that is displayed when the list is empty
+
+    private TextView mEmptyStateTextView;
+    private static int noInternet =-1;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //TODO:DONE
+
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (!(networkInfo != null && networkInfo.isConnected())) {
+
+            mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+            mEmptyStateTextView.setText("CONNECTION NOT AVAILABLE");
+        }
+        else {
+
+            TextView trialView = findViewById(R.id.trial_view);
+            trialView.setText("Welcome!!!");
+            noInternet = 0;
+            mFirebaseAuth = FirebaseAuth.getInstance();
+
+            mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                    if (user != null) {
+                        //Signed in
+
+                        //      onSignedInInitialize(user.getDisplayName());
+
+                    } else {
+                        //Signed out
+
+                        //    onSignedOutCleanup();
+
+                        startActivityForResult(
+                                AuthUI.getInstance()
+                                        .createSignInIntentBuilder()
+                                        .setIsSmartLockEnabled(false)
+                                        .setAvailableProviders(Arrays.asList(
+                                                new AuthUI.IdpConfig.EmailBuilder().build(),
+                                                new AuthUI.IdpConfig.GoogleBuilder().build()
+                                        ))
+                                        .build(),
+                                RC_SIGN_IN);
+                    }
+
+                }
+            };
+        }
     }
+
+//TODO
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(noInternet==0)
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mAuthStateListener != null) {
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+        //        detachDatabaseReadListener();
+        //      mMessageAdapter.clear();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode ==RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Signing In", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Failed Sign In", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+
+
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -58,13 +173,31 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.sign_out_menu) {
             //TODO
+            AuthUI.getInstance().signOut(this);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+
+  /*  private void onSignedInInitialize(String username){
+        mUsername = username;
+        attachDatabaseReadListener();
+
+    }
+
+    private void onSignedOutCleanup(){
+        mUsername=ANONYMOUS;
+        mMessageAdapter.clear();
+        detachDatabaseReadListener();
+    }
+  */
+
+
+
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
